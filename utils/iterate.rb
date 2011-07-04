1e1cff0f0cd54fa2fe1c6b959372238de368877f
Aleksander Pohl <apohllo@o2.pl>
2011-07-04 16:00:03 +0200

    - utils: iterate over texts and query the corpus

---
+++ utils/iterate.rb
@@ -0,0 +1,91 @@
+# encoding: utf-8
+$:.unshift "lib"
+
+require 'srx/polish/sentence'
+require 'benchmark'
+require 'rlp/grammar'
+require 'rlp/corpus'
+require 'ruby-debug'
+
+
+Rlp::Corpus::Corpus.instance.create_database("data/pap2")
+Rlp::Grammar::Client.instance.open_database("data/rlp2",:readonly => false)
+
+#Benchmark.bm do |bm|
+#  bm.report do
+source = Rlp::Corpus::Source.new
+source.name = "Notatki PAP"
+source.store
+SPLIT_RULES = {
+  :word => "\\p{Alpha}\\p{Word}*",
+  :digits => "\\p{Digit}+(?:[:., _/-]\\p{Digit}+)*",
+  :punct => "\\p{Punct}",
+  :graph => "\\p{Graph}",
+  :other => "[^\\p{Word}\\p{Graph}]+"
+}
+
+SPLIT_RE = /#{SPLIT_RULES.values.map{|v| "(?:#{v})"}.join("|")}/m
+SPACE_RE = /#{SPLIT_RULES[:other]}/
+puts SPACE_RE
+puts SPLIT_RE
+forms = {}
+
+
+files = Dir.glob("work/inutf/0000*")
+puts "Total: #{files.size}"
+files.each.with_index do |file_name,index|
+  print "#{index}. "
+  puts if index % 10 == 0
+  document = Rlp::Corpus::Document.new(
+    :title => file_name[file_name.rindex("/")+1..-1],
+    :source => source, :position => index)
+  File.open(file_name) do |file|
+    last_space = ""
+    last_sentence = nil
+    sentence_index = 0
+    SRX::Polish::Sentence.new(file).each do |sentence_str|
+      sentence = Rlp::Corpus::Sentence.new(:document => document)
+      sentence_index += 1
+      document.sentences << sentence
+      unless last_sentence.nil?
+        last_sentence.store
+      end
+      last_sentence = sentence
+
+      last_segment = nil
+      segment_index = 0
+      sentence_str.scan(SPLIT_RE) do |segment_str|
+        #puts segment_str
+        if segment_str =~ SPACE_RE
+          #puts "space"
+          unless last_segment.nil?
+            last_segment.space_after = segment_str
+          end
+          next
+        end
+        segment = Rlp::Corpus::Segment.new(:position => segment_index,
+              :space_after => "")
+        segment.sentence = sentence
+        #if index >= 380 && segment_str == "mandat"
+        #  debugger
+        #end
+        segment.word_form = segment_str
+        segment_index += 1
+        sentence.segments << segment
+        unless last_segment.nil?
+          last_segment.store
+        end
+        last_segment = segment
+      end
+      last_segment.store unless last_segment.nil?
+    end
+    last_sentence.store unless last_sentence.nil?
+  end
+  document.store
+end
+#  end
+#end
+
+Rlp::Grammar::Client.instance.close_database
+Rlp::Corpus::Corpus.instance.close_database
+puts
---
+++ utils/read.rb
@@ -0,0 +1,58 @@
+$:.unshift "lib"
+require 'rlp/corpus'
+require 'rlp/grammar'
+
+include Rlp::Corpus
+
+Corpus.instance.open_database("data/pap2")
+Rlp::Grammar::Client.instance.open_database("data/rlp2")
+
+forms = {}
+counts = Hash.new(0)
+ARGV.each.with_index do |arg,index|
+  flexemes = Rlp::Grammar::Flexeme.find(arg)
+  forms[index] =
+    if flexemes.to_a.empty?
+      [Rlp::Grammar::WordForm.find_by_value(arg)]
+    else
+      flexemes.map{|f| f.word_forms.to_a}
+    end.flatten
+
+  forms[index].each do |form|
+    counts[index] += Segment.find_all_by_form(form).count || 0
+  end
+  if counts[index] == 0
+    puts "Form missing: #{arg}"
+    exit
+  end
+end
+
+counts = counts.sort_by{|k,v| v}
+sentence_ids = {}
+forms[counts[0][0]].each do |form|
+  Segment.find_all_by_form(form).each do |segment|
+    sentence_ids[segment.sentence.rod_id] = [segment.position]
+  end
+end
+
+counts[1..-1].each do |arg_index,index|
+  new_sentence_ids = {}
+  forms[arg_index].each do |form|
+    Segment.find_all_by_form(form).each do |segment|
+      rod_id = segment.sentence.rod_id
+      if sentence_ids.has_key?(rod_id)
+        positions = sentence_ids[rod_id]
+        positions << segment.position
+        new_sentence_ids[rod_id] = positions
+      end
+    end
+  end
+  sentence_ids = new_sentence_ids
+end
+
+sentence_ids.each do |rod_id,positions|
+  puts Sentence.find_by_rod_id(rod_id).to_s(true,positions)
+end
+
+Rlp::Grammar::Client.instance.close_database
+Corpus.instance.close_database
