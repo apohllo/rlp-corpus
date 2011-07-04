342c14ac2121b034a5a367ff2b5312e34d5e7ae7
Aleksander Pohl <apohllo@o2.pl>
2011-07-04 16:01:00 +0200

    - Segment: remove next and prev fields and word form manipulation

--- lib/rlp/corpus/segment.rb
+++ lib/rlp/corpus/segment.rb
@@ -1,52 +1,26 @@
 # encoding: utf-8
+require 'rlp/grammar/text_segment'
 
 module Rlp
   module Corpus
     class Segment < Model
+      include Rlp::Grammar::TextSegment
+
       field :space_after, :string
       field :position, :integer
-      field :letter_case, :string
 
-      has_one :form, :class_name => "Rlp::Grammar::WordForm", :index => :segmented
-      has_one :previous, :class_name => "Rlp::Corpus::Segment"
-      has_one :next, :class_name => "Rlp::Corpus::Segment"
       has_one :sentence
 
-      def word_form=(string)
-        self.letter_case = string.each_char.map do |char|
-          case char
-          when /\p{Lower}/
-            "m"
-          when /\p{Upper}/
-            "M"
-          else
-            "x"
-          end
-        end.join("").sub(/(.)(\1)+$/,"\\1+")
-        string = UnicodeUtils.downcase(string)
-        rlp_form = Rlp::WordForm.find_by_value(string)
-        if rlp_form.nil?
-          rlp_form = Rlp::WordForm.new(:value => string)
-          rlp_form.store
-        end
-        self.form = rlp_form
+      # The next segment in the sentence.
+      def next
+        return nil if self.position == 0
+        self.sentence.segments[self.position-1]
       end
 
-      def word_form
-        self.form.value.each_char.map.with_index do |char,index|
-          case self.letter_case[index]
-          when "M"
-            UnicodeUtils.upcase(char)
-          when "+",nil
-            if self.letter_case[-2] == "M"
-              UnicodeUtils.upcase(char)
-            else
-              char
-            end
-          else
-            char
-          end
-        end.join("")
+      # The previous segment in the sentence.
+      def previous
+        return nil if self.position == self.sentence.segments.count - 1
+        self.sentence.segments[self.position+1]
       end
     end
   end
