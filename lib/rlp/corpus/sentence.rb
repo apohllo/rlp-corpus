f6853b651cd91b495cace648f08f64b7f2bf0bb7
Aleksander Pohl <apohllo@o2.pl>
2011-07-04 16:01:31 +0200

    - Sentence: remove next and prev fields

--- lib/rlp/corpus/sentence.rb
+++ lib/rlp/corpus/sentence.rb
@@ -6,8 +6,6 @@ module Rlp
       field :position, :integer
 
       has_one :document
-      has_one :previous, :class_name => "Rlp::Corpus::Sentence"
-      has_one :next, :class_name => "Rlp::Corpus::Sentence"
       has_many :segments
 
       def to_s(raw=true,hl_indices=[])
@@ -21,6 +19,18 @@ module Rlp
         previous = self.previous ? self.previous.to_s : ""
         next_s = self.next ? self.next.to_s : ""
         "#{previous[-10..-1]}[#{simple}]#{next_s[0..10]}"
+      end
+
+      # The next sentence in the document.
+      def next
+        return nil if self.position == 0
+        self.document.sentences[self.position-1]
+      end
+
+      # The previous sentence in the document.
+      def previous
+        return nil if self.position == self.documents.sentences.count - 1
+        self.document.sentences[self.position+1]
       end
     end
   end
